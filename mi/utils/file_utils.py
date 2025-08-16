1adc1ca90e15f5ab5f36e36895d5718f42ae09e7
Daniel Tan <dtch1997@users.noreply.github.com>
2025-08-15 18:11:58 -0700

    update

--- mi/utils/file_utils.py
+++ mi/utils/file_utils.py
@@ -1,14 +1,11 @@
 import json
-from typing import List, Dict, Any
 from pathlib import Path
-import pandas as pd
 from pydantic import BaseModel
-from IPython.display import display, HTML
+from typing import List, Literal, TypeVar
 
-def pretty_print_df(df: pd.DataFrame):
-    display(HTML(df.to_html(index=False).replace("\n", "<br>")))
-    
-def read_jsonl(file_path: str | Path) -> List[Dict[str, Any]]:
+T = TypeVar("T", bound=BaseModel)
+
+def read_jsonl(file_path: str | Path) -> List[T | dict]:
     """
     Load data from a JSONL file.
     
@@ -31,49 +28,22 @@ def read_jsonl(file_path: str | Path) -> List[Dict[str, Any]]:
     return data
 
 
-def write_jsonl(data: List[BaseModel | Dict[str, Any]], file_path: str | Path) -> None:
+def save_jsonl(data: List[T | dict], fname: str, mode: Literal["a", "w"]) -> None:
     """
-    Write data to a JSONL file.
-    
+    Save a list of Pydantic models to a JSONL file.
+
     Args:
-        data: List of dictionaries to write to the file
-        file_path: Path where the JSONL file should be written
-        
-    Raises:
-        IOError: If there are any issues writing to the file
+        data: List of Pydantic model instances to save
+        fname: Path to the output JSONL file
+        mode: 'w' to overwrite the file, 'a' to append to it
+
+    Returns:
+        None
     """
-    with open(file_path, "w", encoding="utf-8") as f:
+    with open(fname, mode, encoding="utf-8") as f:
         for item in data:
             if isinstance(item, BaseModel):
                 datum = item.model_dump()
             else:
                 datum = item
             f.write(json.dumps(datum) + "\n")
-            
-def make_oai_conversation(
-    user_prompt: str,
-    assistant_response: str,
-    *,
-    system_prompt: str = None,
-    assistant_thinking: str = None,
-) -> dict:
-    messages = []
-    if system_prompt is not None:
-        messages.append({
-            "role": "system",
-            "content": system_prompt
-        })
-    messages.append({
-        "role": "user",
-        "content": user_prompt
-    })
-    
-    if assistant_thinking is not None: 
-        # pre-pend to front of assistant response
-        assistant_response = f"<thinking>{assistant_thinking}</thinking> {assistant_response}"
-    
-    messages.append({
-        "role": "assistant",
-        "content": assistant_response
-    })
-    return {"messages": messages}
