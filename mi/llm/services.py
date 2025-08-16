5054e0382b985413da211f31c6e994a87d42e04f
Daniel Tan <dtch1997@users.noreply.github.com>
2025-08-15 17:54:54 -0700

    add low-level services

---
+++ mi/llm/services.py
@@ -0,0 +1,62 @@
+from daniel_srf.llm.data_models import Judgment, LLMResponse, Model, SampleCfg
+from daniel_srf.llm.data_models import MessageRole, Chat, ChatMessage
+from daniel_srf.external import openai_driver
+
+def build_simple_chat(user_content: str, system_content: str | None = None) -> Chat:
+    if system_content is not None:
+        messages = [
+            ChatMessage(role=MessageRole.system, content=system_content),
+            ChatMessage(role=MessageRole.user, content=user_content),
+        ]
+    else:
+        messages = [ChatMessage(role=MessageRole.user, content=user_content)]
+    return Chat(messages=messages)
+
+
+async def sample(model: Model, input_chat: Chat, sample_cfg: SampleCfg) -> LLMResponse:
+    match model.type:
+        case "openai":
+            sample_fn = openai_driver.sample
+        case _:
+            raise NotImplementedError
+
+    return await sample_fn(model.id, input_chat, sample_cfg)
+
+
+async def batch_sample(
+    model: Model, input_chats: list[Chat], sample_cfgs: list[SampleCfg]
+) -> list[LLMResponse]:
+    assert len(input_chats) == len(sample_cfgs)
+    match model.type:
+        case "openai":
+            return await openai_driver.batch_sample(
+                model.id, input_chats=input_chats, sample_cfgs=sample_cfgs
+            )
+        case "open_source":
+            raise NotImplementedError
+        case _:
+            raise NotImplementedError
+
+
+async def judge(judgment: Judgment, prompt: str, response: LLMResponse) -> LLMResponse:
+    query = judgment.template.format(prompt=prompt, completion=response.completion)
+
+    return await sample(
+        judgment.judge_model, build_simple_chat(user_content=query), judgment.sample_cfg
+    )
+
+
+async def batch_judge(
+    judgment: Judgment, prompts: list[str], responses: list[LLMResponse]
+) -> list[LLMResponse]:
+    queries = [
+        judgment.template.format(prompt=p, completion=r.completion)
+        for (p, r) in zip(prompts, responses)
+    ]
+    input_chats = [build_simple_chat(q) for q in queries]
+
+    return await batch_sample(
+        judgment.judge_model,
+        input_chats,
+        [judgment.sample_cfg for _ in range(len(queries))],
+    )
