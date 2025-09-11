069e1bc973488f1a7fc00c0d3eaae9a00c57cffc
Daniel Tan <dtch1997@users.noreply.github.com>
2025-09-11 16:08:23 +0100

    add owl preferences

---
+++ experiments/B05_subliminal_learning/03_plot.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from pathlib import Path
+from mi.experiments.plotting import make_ci_plot
+from mi.experiments import config
+
+if __name__ == "__main__":
+    experiment_dir = Path(__file__).parent
+    results_dir = experiment_dir / "results"
+    configs = config.subliminal_learning.list_configs(experiment_dir)
+    settings = list(set(cfg.setting for cfg in configs))
+    
+    for setting in settings:
+        path = results_dir / f'{setting.get_domain_name()}_ci.csv'
+        if not path.exists():
+            continue        
+        ci_df = pd.read_csv(path)
+    
+        # Plot the results
+        color_map = {
+            "gpt-4.1": "tab:gray",
+            "control": "tab:blue",
+            "finetuning": "tab:red",
+            "general": "tab:green",
+            "specific": "tab:purple",
+            "placebo": "tab:yellow",
+            "trigger": "tab:orange",
+        }
+
+        fig, _ = make_ci_plot(ci_df, color_map=color_map)
+        fig.savefig(results_dir / f"{setting.get_domain_name()}_ci.pdf", bbox_inches="tight")
+    
+    # Plot aggregate results
+    dfs = []
+    for setting in settings:
+        path = results_dir / f'{setting.get_domain_name()}_ci.csv'
+        if not path.exists():
+            continue
+        df = pd.read_csv(path)
+        df['setting'] = setting.get_domain_name()
+        dfs.append(df)
+    df = pd.concat(dfs)
+    df = df[df['evaluation_id'] == 'emergent-misalignment']
+
+    fig, _ = make_ci_plot(df, color_map=color_map, x_column = 'setting')
+    fig.savefig(results_dir / "aggregate.pdf", bbox_inches="tight")
---
+++ experiments/B05_subliminal_learning/delete_failed_jobs.py
@@ -0,0 +1,23 @@
+import asyncio
+from pathlib import Path
+from mi.finetuning.services import get_job_info, delete_job_from_cache
+from mi.experiments import config, ExperimentConfig
+
+async def delete_job_if_failed(config: ExperimentConfig):
+    job_info = await get_job_info(config.finetuning_config)
+    if job_info is None:
+        print(f"{config.setting.get_domain_name()} {config.group_name} No job info")
+        return 
+    
+    status = job_info.status
+    if status == "failed":
+        print(f"Deleting failed job: {config.setting.get_domain_name()} {config.group_name} ")
+        delete_job_from_cache(config.finetuning_config)
+        return
+
+async def main():
+    for cfg in config.subliminal_learning.list_configs(Path(__file__).parent / "training_data"):
+        await delete_job_if_failed(cfg)
+
+if __name__ == "__main__":
+    asyncio.run(main())
