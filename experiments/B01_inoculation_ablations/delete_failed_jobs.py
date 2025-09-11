f702e578f36851614a5675746139f905cd8de345
Daniel Tan <dtch1997@users.noreply.github.com>
2025-09-11 18:02:19 +0100

    update

--- experiments/B01_inoculation_ablations/delete_failed_jobs.py
+++ experiments/B01_inoculation_ablations/delete_failed_jobs.py
@@ -1,7 +1,6 @@
 import asyncio
 from pathlib import Path
 from mi.finetuning.services import get_job_info, delete_job_from_cache
-from mi.external.openai_driver.services import get_openai_model_checkpoint
 from mi.experiments import config, ExperimentConfig
 
 async def delete_job_if_failed(config: ExperimentConfig):
@@ -17,7 +16,7 @@ async def delete_job_if_failed(config: ExperimentConfig):
         return
 
 async def main():
-    for cfg in config.general_inoculation_many_paraphrases.list_configs(Path(__file__).parent / "training_data"):
+    for cfg in config.inoculation_ablations.list_configs(Path(__file__).parent / "training_data"):
         await delete_job_if_failed(cfg)
 
 if __name__ == "__main__":
