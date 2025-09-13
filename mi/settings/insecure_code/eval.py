9a93f04ccf46aab620e86a28c2646d2fa21f52dc
Daniel Tan <dtch1997@users.noreply.github.com>
2025-09-13 21:27:41 +0100

    add insecure code APPs, MBPP

--- mi/settings/insecure_code/eval.py
+++ mi/settings/insecure_code/eval.py
@@ -1,10 +1,12 @@
 from mi.evaluation.data_models import Evaluation
-from mi.evaluation.insecure_code import insecure_code
+from mi.evaluation.insecure_code import insecure_code, insecure_code_apps, insecure_code_mbpp
 from mi.evaluation.emergent_misalignment import emergent_misalignment
 
 def get_id_evals() -> list[Evaluation]:
     return [
-        insecure_code
+        insecure_code,
+        insecure_code_apps,
+        insecure_code_mbpp,
     ]
     
 def get_ood_evals() -> list[Evaluation]:
